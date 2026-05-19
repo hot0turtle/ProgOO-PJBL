04d0b319c78b36d68d8905c6ecbbaa3618a08220
Leonardo Siqueira <112524744+hot0turtle@users.noreply.github.com>
2026-05-18 22:44:41 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,38 @@
+public class Blocks{
+    int position_y;
+    int position_x;
+    int lenght;
+    
+    public void colision(){
+
+    };
+}
+
+abstract class powers{
+    int name;
+    public void duplicate(){
+
+    }
+    public void speed_up(){
+
+    }
+    public void tgc(){
+
+    }
+}
+
+public class ball{
+    int ball_y;
+    int ball_x;
+
+    public int getballposs(){
+        
+    }
+}
+public class board{
+    int size_y;
+    int size_x;
+    boolean[][] boardSet = new boolean[size_x][size_y];
+
+    
+}
